b39eb69da285ed815333dfc042d0e5b11ae36430
Alexsandr <artamoshkinas@gmail.com>
2022-12-08 14:47:28 +0700

    added image file

--- Task1.py
+++ Task1.py
@@ -1,1 +1,5 @@
-
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+def deletion_of_words(text: str, find_chars: str) -> str:
+    return [i for i in text.split() if find_chars not in i]
+print(deletion_of_words(text='проибабвшцуоашц дацоашуоц вйлцвлйцхабвд лащула',
+                  find_chars = 'абв'))
