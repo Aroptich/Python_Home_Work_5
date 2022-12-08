f6a9ece08f4208f559fe483fe36d039ef9e0456d
Alexandr <artamoshkinas@gmail.com>
2022-12-08 20:45:44 +0700

    Задание 4 исправлено

--- Task4.py
+++ Task4.py
@@ -1,31 +1,40 @@
- def RLE_algoritm(data):
-     encoding = ''
-     previous_character = ''
-     count = 1
-     if not data:
-         return ''
-     for char in data:
-         if char != previous_character:
-             if previous_character:
-                 encoding += str(count) + previous_character
-             count = 1
-             previous_character = char
-         else:
-             count += 1
+def RLE_algoritm(data: str) -> str:
+    """Функция на воход получает строку из последовательность символов.
+    Обрабатывает на количество повторений и выдает сжатое состояние строки"""
+    encoding = ''
+    previous_character = ''
+    count = 1
+    if not data:
+        return ''
+    for char in data:
+        if char != previous_character:
+            if previous_character:
+                encoding += str(count) + previous_character
+            count = 1
+            previous_character = char
+        else:
+            count += 1
+
     else:
-         encoding += str(count) + previous_character
-         return encoding
+        encoding += str(count) + previous_character
+    return encoding
 
- print(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv'))
+print(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv'))
 
- def RLE_decode(data):
-     decode = ''
-     count = ''
-     for char in data:
-         if char.isdigit():
-             count += char
-         else:
-             decode += char * int(count)
-             count = ''
-     return decode
- print(RLE_decode(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv')))
+
+def RLE_decode(data):
+    """Функция на вход получает сжатое состояние строки по методу RLE.
+    Обрабатывает количество повторений каждого символа в строке и
+    выводит развернутое состояние строки(несжатое)"""
+    decode = ''
+    count = ''
+    for char in data:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+
+print(RLE_decode(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv')))
