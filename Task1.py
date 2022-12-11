93cc99628b6025e1e9a061a5a333457b64969cc4
Alexandr <artamoshkinas@gmail.com>
2022-12-11 14:45:34 +0700

    Задание 1 доделано

--- Task1.py
+++ Task1.py
@@ -1,6 +1,10 @@
 #Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
-def deletion_of_words(text: str, find_chars: str) -> str:
+def deletion_of_words(find_chars: str) -> str:
     '''Функция получает на вход строку, в которой ищем и удаляем слова в которые входят искомые символы/слова  '''
-    return ' '.join([i for i in text.split() if find_chars not in i])
-print(deletion_of_words(text='проибабвшцуоашц дацоашуоц вйлцвлйцхабвд лащула',
-                  find_chars = 'абв'))
+    with open('text.txt','r', encoding='UTF-8') as inputtext:
+        reading_file = inputtext.read()
+        text = ' '.join([i for i in reading_file.split() if find_chars not in i])
+        with open('text1.txt', 'w', encoding='UTF-8') as outputtext:
+            outputtext.writelines(text)
+
+deletion_of_words(find_chars = 'абв')
