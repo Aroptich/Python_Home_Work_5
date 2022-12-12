68345d869877bee7e2140090259954442ea9f8b8
Alexsandr <artamoshkinas@gmail.com>
2022-12-12 09:25:47 +0700

    Задание 4 доделано

--- Task4.py
+++ Task4.py
@@ -1,40 +1,47 @@
-def RLE_algoritm(data: str) -> str:
+def RLE_algoritm() -> str:
     """Функция на воход получает строку из последовательность символов.
     Обрабатывает на количество повторений и выдает сжатое состояние строки"""
-    encoding = ''
-    previous_character = ''
-    count = 1
-    if not data:
-        return ''
-    for char in data:
-        if char != previous_character:
-            if previous_character:
-                encoding += str(count) + previous_character
-            count = 1
-            previous_character = char
+
+    with open('textTask4.txt', 'r', encoding='UTF-8') as intext:
+        data = intext.readline()
+        encoding = ''
+        previous_character = ''
+        count = 1
+        if not data:
+            return ''
+        for char in data:
+            if char != previous_character:
+                if previous_character:
+                    encoding += str(count) + previous_character
+                count = 1
+                previous_character = char
+            else:
+                count += 1
+
         else:
-            count += 1
-
-    else:
-        encoding += str(count) + previous_character
+            encoding += str(count) + previous_character
+        with open('text_encoding.txt', 'w', encoding='UTF-8') as outtext:
+            outtext.writelines(encoding)
     return encoding
+print(RLE_algoritm())
 
-print(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv'))
 
-
-def RLE_decode(data):
+def RLE_decode():
     """Функция на вход получает сжатое состояние строки по методу RLE.
     Обрабатывает количество повторений каждого символа в строке и
     выводит развернутое состояние строки(несжатое)"""
-    decode = ''
-    count = ''
-    for char in data:
-        if char.isdigit():
-            count += char
-        else:
-            decode += char * int(count)
-            count = ''
+    with open ('text_encoding.txt', 'r', encoding='UTF-8') as text:
+        data = text.readline()
+        decode = ''
+        count = ''
+        for char in data:
+            if char.isdigit():
+                count += char
+            else:
+                decode += char * int(count)
+                count = ''
+        with open('text_dencoding.txt', 'w', encoding='UTF-8') as text_decoding:
+            text_decoding.writelines(decode)
+
     return decode
-
-
-print(RLE_decode(RLE_algoritm('wwwwiiiiJJJ;;;TTTuudddccbv')))
+print(RLE_decode())
